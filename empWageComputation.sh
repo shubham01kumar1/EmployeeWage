1390a50bda3a002a256b1e0d3a89653de1840114
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-07-18 18:32:38 +0530

    Storing Employee daily wage record along with total salary

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+declare -a dailyWage
+
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF_TIME=2
 WAGE_PER_HR=20
@@ -7,32 +9,39 @@ MAX_WORK_DAYS=20
 MAX_WORK_HR=70
 
 totalWorkHR=0
-day=1
+day=0
 
 function getWorkHR(){
-	local emmpCheck=$1
+	local emp=$1
 	local empHrs=0
 	#selection
-	case $empCheck in
-	$IS_PRESENT_FULL_TIME)
-		empHrs=8 ;;
-	$IS_PRESENT_HALF_TIME)
-		empHrs=4 ;;
-	*)
-		empHrs=0 ;;
+	case $emp in
+		$IS_PRESENT_FULL_TIME)
+			empHrs=8;;
+		$IS_PRESENT_HALF_TIME)
+			empHrs=4;;
+		*)
+			empHrs=0;;
 	esac
 	echo $empHrs
 }
 
-while [ $day -le $MAX_WORK_DAYS ] && [ $totalWorkHR -lt $MAX_WORK_HR ]
+function getEmpWage() {
+	local empHr=$1
+	echo $((empHr * WAGE_PER_HR))
+}
+while [ $day -lt $MAX_WORK_DAYS ] && [ $totalWorkHR -lt $MAX_WORK_HR ]
 do
 	#variable
 	empCheck=$(( RANDOM % 3 ))
-	((day++))
 	#calling a function
-	empHrs="$(getWorkHR $empCheck)"
+	empHrs="$( getWorkHR $empCheck )"
 	#calculation
-	totalWorkHR=$((totalWorkHR + empHrs ))
-	salary=$(( empHrs * WAGE_PER_HR ))
+	totalWorkHR=$(( totalWorkHR + empHrs ))
+	#storing daily
+	dailyWage[$day]="$(getEmpWage $empHrs)"
+	((day++))
 done
-totalSalary=$(( totalWorkHR * WAGE_PER_HR ))
+
+totalSalary=$((totalWorkHR * WAGE_PER_HR ))
+echo ${dailyWage[@]}
