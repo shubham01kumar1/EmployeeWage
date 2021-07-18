4edcdcd3ad47cd156156d18df4082937092a0ea0
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-07-18 18:17:16 +0530

    Employee get working hours function feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
 
-# contant
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF_TIME=2
 WAGE_PER_HR=20
@@ -9,22 +8,31 @@ MAX_WORK_HR=70
 
 totalWorkHR=0
 day=1
+
+function getWorkHR(){
+	local emmpCheck=$1
+	local empHrs=0
+	#selection
+	case $empCheck in
+	$IS_PRESENT_FULL_TIME)
+		empHrs=8 ;;
+	$IS_PRESENT_HALF_TIME)
+		empHrs=4 ;;
+	*)
+		empHrs=0 ;;
+	esac
+	echo $empHrs
+}
+
 while [ $day -le $MAX_WORK_DAYS ] && [ $totalWorkHR -lt $MAX_WORK_HR ]
 do
 	#variable
-	empCheck=$(( RANDOM %3 ))
+	empCheck=$(( RANDOM % 3 ))
 	((day++))
-	#selection
-	case $empCheck in
-		$IS_PRESENT_FULL_TIME )
-			empHrs=8;;
-		$IS_PRESENT_HALF_TIME )
-			empHrs=4;;
-		*)
-			empHrs=0
-	esac
+	#calling a function
+	empHrs="$(getWorkHR $empCheck)"
 	#calculation
 	totalWorkHR=$((totalWorkHR + empHrs ))
 	salary=$(( empHrs * WAGE_PER_HR ))
 done
-totalsalary=$(( totalWorkHR * WAGE_PER_HR ))
+totalSalary=$(( totalWorkHR * WAGE_PER_HR ))
