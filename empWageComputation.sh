4027e0bca8c535c319e359f45194ee8f4c391910
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-07-18 17:27:20 +0530

    Employee Monthly Salary Calculation with Conditions feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,11 +5,15 @@ IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF_TIME=2
 WAGE_PER_HR=20
 MAX_WORK_DAYS=20
+MAX_WORK_HR=70
+
 totalWorkHR=0
-for ((day=1;day<=20;day++))
+day=1
+while [ $day -le $MAX_WORK_DAYS ] && [ $totalWorkHR -lt $MAX_WORK_HR ]
 do
 	#variable
 	empCheck=$(( RANDOM %3 ))
+	((day++))
 	#selection
 	case $empCheck in
 		$IS_PRESENT_FULL_TIME )
